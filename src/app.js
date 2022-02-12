fa214eb1c62f9e01810b7c6d1a0de6d3d368e7d0
Jorge22fv <jorge.22fv@gmail.com>
2022-02-12 00:59:02 +0000

    excuse generator listo

--- src/app.js
+++ src/app.js
@@ -6,6 +6,29 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let whoex = [Math.floor(Math.random() * who.length)];
+  let actionex = [Math.floor(Math.random() * action.length)];
+  let whatex = [Math.floor(Math.random() * what.length)];
+  let whenex = [Math.floor(Math.random() * when.length)];
+
+  let excuse =
+    who[whoex] +
+    " " +
+    action[actionex] +
+    " " +
+    what[whatex] +
+    " " +
+    when[whenex];
+
+  document.getElementById("excuse").innerHTML = excuse;
 };
